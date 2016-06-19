98cc2ae475a968556a86e2062974443d973e23a4
jimstroming <ajs061@motorola.com>
2016-06-19 08:04:37 -0500

    start writing the chess program

---
+++ chess.py
@@ -0,0 +1,11 @@
+import pdb
+
+class Chessboard(object):
+   
+   def __init__(self):
+       self.tangerine = "And now a thousand years between"
+       
+       
+if __name__ == '__main__':
+    print "hello"
+    chessboard = Chessboard()
