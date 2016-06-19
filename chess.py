cf733fbfe69a43713ccbacb00aeec7f490c5ad47
jimstroming <ajs061@motorola.com>
2016-06-19 10:41:30 -0500

    add the pawns

--- chess.py
+++ chess.py
@@ -5,10 +5,12 @@ class Chessboard(object):
    def __init__(self):
        self.tangerine = "And now a thousand years between"
        firstrow = ['WR','WN','WB','WQ','WK','WB','WN','WR']
+       wpawnrow = ['WP','WP','WP','WP','WP','WP','WP','WP']
+       blankrow = ['00','00','00','00','00','00','00','00']       
+       bpawnrow = ['BP','BP','BP','BP','BP','BP','BP','BP']
        lastrow  = ['BR','BN','BB','BQ','BK','BB','BN','BR']
-       blankrow = ['00','00','00','00','00','00','00','00']
-       self.board = [firstrow, blankrow, blankrow, blankrow,
-                     blankrow, blankrow, blankrow, lastrow]
+       self.board = [firstrow, wpawnrow, blankrow, blankrow,
+                     blankrow, blankrow, bpawnrow, lastrow]
                      
    def printboard(self):
        for x in range(0,8):
