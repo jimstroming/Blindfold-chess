5e289a83d01497ece776d2935e324d4dda206106
jimstroming <ajs061@motorola.com>
2016-06-19 21:22:46 -0500

    add move rules

--- chess.py
+++ chess.py
@@ -11,6 +11,19 @@ class Chessboard(object):
        self.board = [firstrow, wpawnrow, blankrow, blankrow,
                      blankrow, blankrow, bpawnrow, lastrow]
                      
+       self.moverules = {'R':[[0,7],[0,-7],[7,0],[-7,0]],
+                         'B':[[7,7],[7,-7],[-7,7],[-7,-7]],
+                         'Q':[[0,7],[0,-7],[7,0],[-7,0],
+                              [7,7],[7,-7],[-7,7],[-7,-7]],
+                         'K':[[0,1],[0,-1],[1,0],[-1,0],
+                              [1,1],[1,-1],[-1,1],[-1,-1]],
+                         'N':[[2,1],[2,-1],[-2,1],[-2,-1],
+                              [1,2],[1,-2],[-1,2],[-1,-2]],
+                         'pawnnocapture':[[0,1]],
+                         'pawncapture':[[-1,1],[1,1]],
+                         'pawnfirstmove':[[0,1],[0,2]]
+                         }
+
    def printboard(self):
        for x in range(0,8):
            print self.board[7-x]                  
@@ -18,5 +31,5 @@ class Chessboard(object):
 if __name__ == '__main__':
     print "hello"
     chessboard = Chessboard()
-    print chessboard.tangerine
     chessboard.printboard()
+    print chessboard.moverules['B']
